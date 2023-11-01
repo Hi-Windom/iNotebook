a863979e99674861c975665b44a9349d33f2f61c
Soltus <liaoshanyi@qq.com>
2023-11-01 20:59:25 +0800

    6

---
+++ jupyter/jupyter/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace jupyter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void logout_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void 项目管理_Click(object sender, RoutedEventArgs e)
+        {
+            //WindowsManager<八价兲谛全丰专区>.Show(new object()); // 单实例
+            //this.Hide();
+        }
+        private void 智能任务_Click(object sender, RoutedEventArgs e)
+        {
+            //WindowsManager<八价兲谛全丰专区>.Show(new object()); // 单实例
+            //this.Hide();
+        }
+        private void 控制中心_Click(object sender, RoutedEventArgs e)
+        {
+            //WindowsManager<八价兲谛全丰专区>.Show(new object()); // 单实例
+            //this.Hide();
+        }
+        private void 关于软件_Click(object sender, RoutedEventArgs e)
+        {
+            //WindowsManager<八价兲谛全丰专区>.Show(new object()); // 单实例
+            //this.Hide();
+        }
+    }
+}
